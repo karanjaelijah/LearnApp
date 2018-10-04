ca14a65fbaa438bce7478109afd8a354f98f290f
karanjaelijah <karanjaelijah0@gmail.com>
2018-10-04 17:38:33 +0300

    Initial commit

---
+++ mysite/polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from  .models import Question, Choice
+# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                {'fieds': ['qustion_text']}),
+        ('Date information',  {'felds': ['pub_date']}),
+['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+admin.site.register(Question, QuestionAdmin)
+
+
